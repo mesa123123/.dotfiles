d832be600c825b058d09843075505e1e70eacc88
mesa123123 <bowmanpete123@gmail.com>
2020-09-02 17:57:01 +1200

    Sessions End Wed Sep  2 17:57:01 NZST 2020

--- remote_sync.sh
+++ remote_sync.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-git add .
-commit_message="Sessions End $(date)"
-echo $commit_message
-git commit -m "${commit_message}"
-git push origin master
+# Process Command Switches
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+# Sync Mode Switch	
+	-m|--MODE)
+	MODE="$2"
+	shift # past argument
+	shift # past value
+	;;
+# All other switches
+	*)
+	POSITIONAL+=("$1") # save it in an array for later
+	shift # past argument
+	;;
+esac
+done
+
+# Sync all changes from other clients when things start up
+if [[ "${MODE}" == "begin" ]]; then
+	echo "Startup"
+fi
+
+# move all changes that have been made during the session to the cloud
+if [[ "${MODE}" == "end" ]]; then
+	# Regular Git Push Routine	
+	git add .
+	# DateTime Stamped Commit
+	commit_message="Sessions End $(date)"
+	echo $commit_message
+	git commit -m "${commit_message}"
+	git push origin master
+fi
+
 
