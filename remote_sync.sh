11afebd180f85fb125d40c2fa31c5b6febf9baf4
mesa123123 <bowmanpete123@gmail.com>
2020-09-02 17:17:51 +1200

    Sessions End Wed Sep  2 17:17:51 NZST 2020

--- remote_sync.sh
+++ remote_sync.sh
@@ -5,3 +5,6 @@ commit_message="Sessions End $(date)"
 echo $commit_message
 git commit -m "${commit_message}"
 git push origin master
+
+expect "Username for 'https://github.com':"
+send -- "mesa123123"
