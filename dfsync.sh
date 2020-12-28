e7fb4df382be70908a8e351ae722a7cb0224ed72
mesa123123 <bowmanpete123@gmail.com>
2020-12-28 16:16:00 +1300

    Sessions End Mon Dec 28 16:16:00 NZDT 2020

--- dfsync.sh
+++ dfsync.sh
@@ -7,7 +7,7 @@
 
 VERSION=0.1.0
 SUBJECT="Dotfile Sync Script"
-USAGE="Usage: remote_dotfiles_sync.sh -m args"
+USAGE="Usage: remote_dotfiles_sync.sh -m begin/end -r true/false"
 LOGFILE=~/.dotfiles/synclogs.log
 
 # ------- Global Script Variables -------
@@ -48,7 +48,7 @@ fi
 # --------
 
 # --------- Help Text ---------
-HELP_MESSAGE="Proper uses of this scripts are:\n\nSwitch -m\n\tbegin:\n\t\tsets up the dotfiles for the client\n\tend:\n\t\tsync up changes to the dotfiles to the github repo"
+HELP_MESSAGE="Proper uses of this scripts are:\n\nSwitch -m\n\tbegin:\n\t\tsets up the dotfiles for the client\n\tend:\n\t\tsync up changes to the dotfiles to the github repo -r\n\t checks for ~/.repos file and also syncs up the named local repos with their chosen remotes"
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 	echo -e "${HELP_MESSAGE}"
@@ -190,14 +190,15 @@ function start_browser_proxy()
 # OUTS: None
 function pullRepo()
 {
-	git -C "${1}" fetch >> $LOGFILE;
-	git -C "${1}" pull "${2}" "${3}" -q >> $LOGFILE;
+    echo "Still here$1 $2 $3"
+	git -C $1 fetch >> $LOGFILE;
+	git -C $1 pull $2 $3 -q >> $LOGFILE;
 }
 
 # NAME: timestamppush
 # DESC: Pushes repos that I want updated on all clients to push back to the cloud at the end of the session
 # ARGS: $@ (optional):  1, Path to git repo; 2, Remote Repo Name; 3, Remote Branch Name; 
-# OUTS: 
+# OUTS: None
 function timeStampPush()
 {
 	# Regular Git Push Routine
@@ -211,15 +212,18 @@ function timeStampPush()
 
 # NAME: getRepoInfo
 # DESC: Takes Repo file and puts in the form of an assocaite array 
-# ARGS: $@ (optional): None
-# OUTS: List of Lists containing: 1, repo path; 2, Remote Repo Name (e.g. origin); 3, Remote Branch Name;
+# ARGS: $@ (optional): $1, The file path of the repos file
+# OUTS: None, but it will echo the lines back to the parent function calling it, best used when assigning something to an array
 function getRepoInfo()
 {
-  # Declare an Array for the repos
   # Read ~/.repo file line by line
-  # For each line put it onto the end of the array
-  # Return the Array
-  echo "TODO THIS FUNCTION"
+  while IFS= read -r line; do
+    # For each line echo it out to the array calling it 
+      if [[ ! "$line" == "#"* ]]; then
+          NEWITEM="$line"
+          echo $NEWITEM | sed 's/ /,/g' 
+      fi
+  done < $1
 }
 
 # --------- End of Reuseable Methods --------
@@ -246,11 +250,17 @@ if [[ "${MODE}" == "begin" ]]; then
     fi
     # if there are other repos that need to be synced up then pull them too
     if [[ "${REPOS}" == "true" ]]; then
-        REPOLIST=getRepoInfo
-        for REPO in ${REPOSLIST[@]}; do
-            declare -A REPOArr=($REPO) 
-            pullRepo "${REPOArr[1]}" "${REPOArr[2]}" "${REPOArr[3]}"
-        done
+        if [[ -f ~/.repos ]]; then
+            REPOLIST=( $(getRepoInfo "$HOME/.repos") )
+            for REPO in ${REPOLIST[@]}; do
+                REPOArr=( $(echo "${REPO}" | sed 's/,/ /g') )
+                pullRepo ${REPOArr[1]} ${REPOArr[2]} ${REPOArr[3]}
+                echo "Repo Pulled ${REPOArr[0]}" >> $LOGFILE
+            done
+        else
+            echo "REPOS NOT SYNCED, YOU NEED TO CREATE A REPOS FILE!" >> $LOGFILE
+            exit 1;
+        fi
     fi    
 fi
 
@@ -262,11 +272,17 @@ if [[ "${MODE}" == "end" ]]; then
     timeStampPush ~/.dotfiles origin master 
     # Push All Other Git Repos
     if [[ "${REPOS}" == "true" ]]; then
-        REPOLIST=getRepoInfo
-        for REPO in ${REPOSLIST[@]}; do
-            declare -A REPOArr=($REPO) 
-            timeStampPush "${REPOArr[1]}" "${REPOArr[2]}" "${REPOArr[3]}"
-        done
+        if [[ -f ~/.repos ]]; then
+            REPOLIST=( $(getRepoInfo "$HOME/.repos") )
+            for REPO in ${REPOLIST[@]}; do
+                REPOArr=( $(echo "${REPO}" | sed 's/,/ /g'i) )
+                timeStampPush "${REPOArr[2]}" "${REPOArr[3]}" "${REPOArr[4]}"
+                echo "Repo Pulled ${REPOArr[1]}" >> $LOGFILE
+            done
+        else
+            echo "REPOS NOT SYNCED, YOU NEED TO CREATE A REPOS FILE!" >> $LOGFILE
+            exit 1;
+        fi
     fi    
 fi
 
