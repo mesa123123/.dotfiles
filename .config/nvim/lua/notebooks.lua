761e9dc1057c0af18526ce153ee5d9ccf9e76c69
Peter Bowman <bowmanpete123@gmail.com>
2024-03-25 23:20:47 +0000

    Sessions End Mon 25 Mar 2024 23:20:47 GMT

--- .config/nvim/lua/notebooks.lua
+++ .config/nvim/lua/notebooks.lua
@@ -27,17 +27,17 @@ local norm_keyset = ufuncs.norm_keyset
 -- Notebook Setup - quarto.nvim, molten-nvim: <leader>n
 --------------------------------
 
-local quarto = require("quarto")
-quarto.setup({
-	codeRunner = {
-		enables = true,
-		ft_runners = { python = "molten" },
-	},
-})
-
-norm_keyset(lm.notebook .. "p", "QuartoPreview", "Open Notebook Preview")
--- norm_keyset(lm.notebook .. "x", 'MoltenInit', "Initalize Notebook Kernel")
--- norm_keyset(lm.notebook_kernel .. "i", 'MoltenInfo', "Kernel Info")
--- norm_keyset(lm.notebook .. "q", 'MoltenDeinit', "Shutdown Kernel")
-vim.keymap.set("n", "<localleader>ni", ":MoltenInit<CR>",
-    { silent = true, desc = "Initialize the plugin" })
+-- local quarto = require("quarto")
+-- quarto.setup({
+-- 	codeRunner = {
+-- 		enables = true,
+-- 		ft_runners = { python = "molten" },
+-- 	},
+-- })
+--
+-- norm_keyset(lm.notebook .. "p", "QuartoPreview", "Open Notebook Preview")
+-- -- norm_keyset(lm.notebook .. "x", 'MoltenInit', "Initalize Notebook Kernel")
+-- -- norm_keyset(lm.notebook_kernel .. "i", 'MoltenInfo', "Kernel Info")
+-- -- norm_keyset(lm.notebook .. "q", 'MoltenDeinit', "Shutdown Kernel")
+-- vim.keymap.set("n", "<localleader>ni", ":MoltenInit<CR>",
+--     { silent = true, desc = "Initialize the plugin" })
