6ba0de593fe76bbf965f97f1f0e2c8b202a6843e
Peter Bowman <bowmanpete123@gmail.com>
2024-04-07 23:36:35 +0100

    Sessions End Sun 07 Apr 2024 23:36:35 BST

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -66,7 +66,7 @@ require("package_setup")
 require("notebooks")
 require("lsp")
 require("options")
-
+require("line")
 ----------
 
 -- Extra Vars
@@ -195,6 +195,7 @@ cmd("colorscheme theme")
 ----------
 gv["rainbow_active"] = 1
 ----------
+
 ----------
 local devIcons = require("nvim-web-devicons")
 devIcons.setup({
@@ -572,168 +573,6 @@ keymap.set("n", "<leader>:", ":lua print(", keyopts({ desc = "Print Lua Command"
 keymap.set("n", "<leader>;", ":h ", keyopts({ desc = "Open Help Reference" }))
 keymap.set("n", "<leader>!", ":! ", keyopts({ desc = "Run System Command" }))
 
-----------
-
------------------------------
--- Window Line - barbecue
------------------------------
-
--- Setup
-----------
-require("barbecue").setup()
-----------
-
------------------------------
--- Status Line - lualine
------------------------------
-
-----------
--- Helper Functions
-----------
-
--- Get accurate time on panel
-----------
-function Zonedtime(hours)
-  -- Change time zone here (default seems to be +12 on home workstation)
-  local zone_difference = 11
-  local t = os.time() - (zone_difference * 3600)
-  local d = t + hours * 3600
-  return os.date("%H:%M %Y-%m-%d", d)
-end
-----------
-
--- Truncate components on smaller windows
-------------------
-local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
-  return function(str)
-    local win_width = vim.fn.winwidth(0)
-    if hide_width and win_width < hide_width then
-      return ""
-    elseif trunc_width and trunc_len and win_width < trunc_width and #str > trunc_len then
-      return str:sub(1, trunc_len) .. (no_ellipsis and "" or "...")
-    end
-    return str
-  end
-end
-------------------
-
--- Linting, Formatting, Lsp, Dap Info
-------------------
--- LSP
-local active_lsp = {
-  function()
-    local bufnr = vim.api.nvim_get_current_buf()
-    local lsps = vim.lsp.get_active_clients({ bufnr })
-    if lsps and #lsps > 0 then
-      local names = {}
-      for _, lsp in ipairs(lsps) do
-        table.insert(names, lsp.name)
-      end
-      return string.format(" %s", table.concat(names, ", "))
-    else
-      return ""
-    end
-  end,
-  color = { fg = palette.bright_red },
-  fmt = trunc(120, 3, 90, true),
-}
--- Formatter
-local active_formatter = {
-  function()
-    local formatters = require("conform").list_formatters_for_buffer(0)
-    if formatters ~= nil then
-      return string.format(" %s", table.concat(formatters, ", "))
-    else
-      return ""
-    end
-  end,
-  color = { fg = palette.bright_green },
-  fmt = trunc(120, 3, 90, true),
-}
--- Linter
-local active_lint = {
-  function()
-    local linters = require("lint").linters_by_ft[vim.bo.filetype][1]
-    if linters ~= nil then
-      return string.format("󰯠 %s", linters)
-    else
-      return ""
-    end
-  end,
-  color = { fg = palette.bright_yellow },
-  fmt = trunc(120, 4, 90, true),
-}
--- Debugger
-local debug_status = {
-  function()
-    local status = require("dap").status()
-    if status ~= "" then
-      return string.format("󰃤 %s", status)
-    else
-      return ""
-    end
-  end,
-  color = { fg = palette.dark_red },
-  fmt = trunc(120, 4, 90, true),
-}
-------------------
-
--- Show if a macro is recording
-------------------
-local noice_recording = {
-  function()
-    local rec_status = vim.fn.reg_recording()
-    if rec_status ~= "" then
-      return string.format("󰑋 %s", string.sub(rec_status, string.len(rec_status)))
-    else
-      return ""
-    end
-  end,
-  color = { fg = palette.dark0_hard },
-}
-------------------
-
--- Config
-----------
-require("lualine").setup({
-  options = {
-    section_separators = { left = " ", right = " " },
-    component_separators = { left = "", right = "" },
-    theme = theme.line_theme
-  },
-  sections = {
-    lualine_a = {
-      {
-        "mode",
-        fmt = function(res)
-          return res:sub(1, 1)
-        end,
-      },
-      noice_recording,
-    },
-    lualine_b = {
-      "branch",
-      {
-        "diff",
-        symbols = { added = "+", modified = "~", removed = "-" },
-        fmt = trunc(120, 10000, 120, true),
-      },
-      {
-        "diagnostics",
-        symbols = { error = "", warn = "", info = "", hint = "" },
-        fmt = trunc(120, 10000, 120, true),
-      },
-    },
-    lualine_c = {
-      { "filetype", colored = true, icon_only = true, icon = { align = "right" }, fmt = trunc(120, 4, 90, true) },
-      debug_status,
-      { "overseer", colored = true },
-    },
-    lualine_x = { active_lsp, active_lint, active_formatter },
-    lualine_y = { { "progress", fmt = trunc(120, 10000, 120, true) }, { "location" } },
-    lualine_z = { "Zonedtime(11)" },
-  },
-})
 ----------
 
 ----------------------------------
@@ -1124,15 +963,18 @@ keymap.set("v", lm.wiki_linkOpts .. "a", "<cmd>ObsidianLink<cr>", { silent = tru
 ---------------------------------
 -- Latex Functionality: <leader>l - Vimtex
 ---------------------------------
+-- TODO: Put into latex settings file
 
 -- Setup (Currently not supported in Lua)
-
+----------
 api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.tex" },
   callback = function()
     vim.g["vimtext_view_method"] = "zathura"
   end,
 })
+----------
+
 ---------------------------------
 -- Code Align
 ---------------------------------
